b308cbb60ebfc2c83465cf95c5ead63a99829fe3
Alejandro  gallegos <alejandrogallegos@MacBook-de-Alejandro.local>
2020-07-13 20:29:58 -0500

    Se agregan cambios para visualizar el buscador en el dashboard

--- src/views/Auth/Dashboard/Dashboard.vue
+++ src/views/Auth/Dashboard/Dashboard.vue
@@ -1,15 +1,58 @@
 <template>
-    <div class="container mt-50">
-       <div class="columns">
-           <div class="column is-12">
-               <h1>Dashboard</h1>
-           </div>
-        </div> 
+  <div class="container mt-50">
+    <div class="columns">
+      <div class="column is-6 is-offset-3">
+        <h3 class="title is-3">Busqueda</h3>
+        <hr />
+        <form action="#" @submit.prevent="buscar()">
+          <div class="field">
+            <label class="label">Buscar</label>
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                placeholder="Buscar"
+                v-model="product"
+              />
+            </div>
+          </div>
+          <button @click="buscar()" class="button is-primary">Buscar</button>
+        </form>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Dashboard'
-}
+  name: "Dashboard",
+  data() {
+    return {
+      product: ""
+    }
+  },
+  methods: {
+    buscar() {
+      if(this.product) {
+        const data = {
+          product: this.product
+        };
+        fetch("endpoint", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log("Data", data)
+        })
+        .catch(error => {
+          console.error("Error: ", error)
+        })
+      }
+    }
+  },
+};
 </script>
